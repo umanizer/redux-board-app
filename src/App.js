0c2ddaeb49d31f6ba29710e6083fd4ee0f14021b
umanizer <arata1005@yahoo.co.jp>
2022-10-28 00:14:39 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "./App.css";
+import { addPost, deletePost } from "./features/posts";
 
 function App() {
+  const postsList = useSelector((state) => state.posts.value);
+  const dispatch = useDispatch();
+  const [inputName, setInputName] = useState("");
+  const [inputContent, setInputContent] = useState("");
+
+  const handleClick = () => {
+    if (inputName === "" || inputContent === "") return;
+    const id =
+      postsList.length === 0 ? 0 : postsList[postsList.length - 1].id + 1;
+    dispatch(addPost({ id: id, name: inputName, content: inputContent }));
+    setInputName("");
+    setInputContent("");
+  };
+  const handleDeleteClick = (id) => {
+    console.log("handleDelete");
+    dispatch(deletePost({id}));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>React-Redux掲示板</h1>
+      </div>
+      <div className="addPost">
+        <input
+          type="text"
+          placeholder="お名前"
+          value={inputName}
+          onChange={(e) => setInputName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="投稿内容"
+          value={inputContent}
+          onChange={(e) => setInputContent(e.target.value)}
+        />
+        <button onClick={handleClick}>投稿</button>
+        <hr />
+      </div>
+      <div className="displayPosts">
+        {postsList.map((post) => (
+          <div key={post.id} className="post">
+            <h1 className="postName">{post.name}</h1>
+            <h1 className="postContent">{post.content}</h1>
+            <button onClick={() => handleDeleteClick(post.id)}>削除</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
